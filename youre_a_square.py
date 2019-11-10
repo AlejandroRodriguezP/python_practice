c26d3f917c5364a6ad9be6acdf7bc944cb244664
Alejandro <a.rodriguez.p.1995@gmail.com>
2019-11-09 18:59:20 -0600

    Completed
    completed toy problem,

--- youre_a_square.py
+++ youre_a_square.py
@@ -7,7 +7,11 @@ in other words, it is the product of some integer with itself.
 
 
 def is_square(n):
-    if n
+    import math
+    root = math.sqrt(n)
+    if int(root + 0.5) ** 2 == n:
         return True
     else:
         return False
+
+print(is_square(0))

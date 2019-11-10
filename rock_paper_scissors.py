ec6deb3af4d2378117af766b0df6455249b43172
Alejandro <a.rodriguez.p.1995@gmail.com>
2019-11-09 18:30:55 -0600

    play_again(COMPLETED)
    play_again() function is working after giving the score of the match.
    program passing all tests

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,16 +4,21 @@ Make a two-player Rock-Paper-Scissors game.
 compare them, print out a message of congratulations to the winner, and ask if the players want to start a new game)
 '''
 
+
 def play_again():
+    # Variables
     wanna_play = input("You wanna play again? (yes / no) ")
+
+    # Logic
     if wanna_play == "yes":
         print("alright, lets play")
         start_game()
     elif wanna_play == "no":
         print("aright,see you soon.")
-        start_game()
+        return False
     else: print("thats not a valid input")
     play_again()
+
 
 def start_game():
 # Variables
@@ -24,43 +29,55 @@ def start_game():
  while player1_moves == "rock":
     if player2_moves == "rock":
         print("TIE ")
+        play_again()
         break
     elif player2_moves == "paper":
         print("Player 2 wins, paper beats rock ")
+        play_again()
         break
     elif player2_moves == "scissors":
         print("Player 1 wins, rock beats scissors")
+        play_again()
         break
     else:
         print("INVALID MOVE")
+        play_again()
         break
 
  while player1_moves == "paper":
     if player2_moves == "rock":
         print("Player 1 wins, paper beats rock ")
+        play_again()
         break
     elif player2_moves == "paper":
         print("TIE")
+        play_again()
         break
     elif player2_moves == "scissors":
         print("Player 2 wins, scissors beat paper...duh!")
+        play_again()
         break
     else:
         print("INVALID MOVE")
+        play_again()
         break
 
  while player1_moves == "scissors":
     if player2_moves == "rock":
         print("Player 2 wins, rock beats paper")
+        play_again()
         break
     elif player2_moves == "paper":
         print("Player 1 wins, scissors beat paper...duh!")
+        play_again()
         break
     elif player2_moves == "scissors":
         print("TIE")
+        play_again()
         break
     else:
         print("INVALID MOVE")
+        play_again()
         break
 
 

267d9c58809c4005aecf29b07cda5773b3576a4a
Alejandro <a.rodriguez.p.1995@gmail.com>
2019-11-13 12:38:26 -0600

    Complete
    completed program, passing all tests on codewars.

---
+++ remove_first_and_last.py
@@ -0,0 +1,19 @@
+'''
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
+You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+'''
+
+
+def remove_char(s):
+    size = len(s)
+    if size <= 2:
+        print(str(size) + " " + s)
+        return " "
+    elif size > 2:
+        f_last = s[1:-1]
+        print(f_last)
+        return f_last
+
+
+remove_char("alejandro")
+

5ac3a25233d5861aaec4b695593ee924e608116a
Alejandro <a.rodriguez.p.1995@gmail.com>
2019-11-27 12:23:33 -0600

    list_o_c Complete
    toy problem completes

--- list_overlap_comprehensions.py
+++ list_overlap_comprehensions.py
@@ -18,4 +18,6 @@ Randomly generate two lists to test this
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-answer = [0 for x in a]
+common = [i for i in range(max(max(a), max(b))) if i in a and i in b]
+
+print(common)
